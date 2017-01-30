c37da75cd9b621084b0c1c3db2dab050b5d9ac6f
Cyrus <cdbalsara@icloud.com>
2017-01-30 11:28:31 +0000

    added event listener to change the number of sets when selected from selectbox

--- public/app.js
+++ public/app.js
@@ -1,9 +1,21 @@
 
-var init = function(){
+var app = function(){
   var tag = document.getElementById('firstP');
   tag.innerHTML = "The gym table with workout options will go in here";
+
+  var selectBox1 = document.getElementById('firstSelect');
+  selectBox1.onchange = handleSelectSetChange;
+
+  
 };
 
-window.onload = init;
+var handleSelectSetChange = function(){
+  var newOption = document.getElementById('set-result');
+  newOption.innerText = this.value;
+}
+
+
+
+window.onload = app;
 
 
