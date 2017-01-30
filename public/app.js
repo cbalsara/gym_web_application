182753e109cdc187b67325dd22a795346290cfd1
Cyrus <cdbalsara@icloud.com>
2017-01-30 11:43:55 +0000

    added event listener to save button when it is clicked

--- public/app.js
+++ public/app.js
@@ -1,23 +1,33 @@
 
 var app = function(){
   var tag = document.getElementById('firstP');
-  tag.innerHTML = "The gym table with workout options will go in here";
+  tag.innerHTML = "Please Enter a Gym Exercise To Use";
 
-  var selectBox1 = document.getElementById('firstSelect');
+  var selectBox1 = document.getElementById('first-Select');
   selectBox1.onchange = handleSelectSetChange;
 
-  var selectBox2 = document.getElementById('secondSelect');
+  var selectBox2 = document.getElementById('second-Select');
   selectBox2.onchange = handleSelectRepChange;
+
+  var button = document.getElementById('save-Button');
+  button.onclick = handleSaveButtonClick;
 };
+
+// -----------------------------------------------------------------
 
 var handleSelectSetChange = function(){
   var newOption = document.getElementById('set-result');
-  newOption.innerText = this.value;
+  newOption.innerText = this.value + ' Sets';
 }
 
 var handleSelectRepChange = function(){
   var newOption = document.getElementById('rep-result');
-  newOption.innerText = this.value;
+  newOption.innerText = this.value + ' Reps';
+}
+
+var handleSaveButtonClick = function(){
+  var saveClick = document.getElementById('save-Text-Result')
+  saveClick.innerText = 'Exercise has been saved!';
 }
 
 
