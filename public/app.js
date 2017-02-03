69c64d76eff0d87cb813467422809ec374a19b49
Cyrus <cdbalsara@icloud.com>
2017-02-03 13:15:42 +0000

    exercise name object being returned on button add click

--- public/app.js
+++ public/app.js
@@ -4,42 +4,34 @@ var app = function(){
   var tag = document.getElementById('firstP');
   tag.innerHTML = "Please Enter a Gym Exercise To Use";
 
-  var selectBox1 = document.getElementById('set-select');
-  selectBox1.onchange = handleSelectSetChange;
+  // var selectBox1 = document.getElementById('set-select');
+  // selectBox1.onchange = handleSelectSetChange;
 
-  var selectBox2 = document.getElementById('rep-select');
-  selectBox2.onchange = handleSelectRepChange;
+  // var selectBox2 = document.getElementById('rep-select');
+  // selectBox2.onchange = handleSelectRepChange;
 
   var button = document.getElementById('add-workout');  
   button.onclick = createWorkout;
-
-
-  // var workout = function(name, weight, set, rep){
-  //   var name = createName(name);
-  //   var weight = createWeight(weight);
-  //   var set = createSet(set);
-  //   var rep = createRep(rep);
-  // }
- 
 };
 
 // -----------------------------------------------------------------
 
-// var createName = function(name){
-//   var exName = document.getElementById('added-result');
-//   exName.innerText = name
-//   return exName;
-// }; 
-
-var createWorkout = function(set, rep){
+var createWorkout = function(name, weight, set, rep){
+  var addName = handleInputExerciseName(name);
   var setList = handleSelectSetChange(set);
   var repList = handleSelectRepChange(rep);
+}
+
+var handleInputExerciseName = function(name){
+  var selected = document.createElement('ul');
+  selected.innerText = 'Name' + name;
+  var a = document.getElementById('added-result');
+  a.appendChild(selected); 
 }
 
 var handleSelectSetChange = function(set){
   var option1 = document.createElement('ul');
   option1.innerText = set + ' Sets';
-
   var a = document.getElementById('added-result');
   a.appendChild(option1);
 }
@@ -47,22 +39,9 @@ var handleSelectSetChange = function(set){
 var handleSelectRepChange = function(rep){
   var option2 = document.createElement('ul');
   option2.innerText = rep + ' Reps';
-
   var a = document.getElementById('added-result');
   a.appendChild(option2); 
 }
-
-
-// var handleAddWorkoutButtonClick = function(){
-//   var saveSetClick = document.getElementById('set-result');
-//   var saveRepClick = document.getElementById('rep-result');
-//   saveSetClick.innerText = handleSelectSetChange();
-//   saveRepClick.innerText = handleSelectRepChange();
-// };
-
-
-
-
 
 window.onload = app;
 
